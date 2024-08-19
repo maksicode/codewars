766cf113f8a57ce4408d542e9c04bea2add5b760
maksicode <maksim-mih23@yandex.ru>
2024-08-19 17:24:07 +0300

    Zadacha 4

---
+++ 8kyu/zadacha4.js
@@ -0,0 +1,13 @@
+// Create a function that takes an integer as an argument and returns 
+// "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    return number % 2 ? 'Odd' : 'Even';
+}
+
+// console.log(evenOrOdd(1));
+// console.log(evenOrOdd(7));
+// console.log(evenOrOdd(16));
+// console.log(evenOrOdd(21));
+// console.log(evenOrOdd(6));
+
