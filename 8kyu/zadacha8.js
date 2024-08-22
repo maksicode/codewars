e1054ac0f8fefe52a8f869aa30fcb30307b948ce
maksicode <maksim-mih23@yandex.ru>
2024-08-22 12:57:37 +0300

    Zadacha 8

---
+++ 8kyu/zadacha8.js
@@ -0,0 +1,18 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+    let result = '';
+    
+    // Перебираем каждый символ в строке
+    for (let i = 0; i < str.length; i++) {
+        // Добавляем символ дважды в результат
+        result += str[i] + str[i];
+    }
+    
+    return result;
+}
+
+// console.log(doubleChar("String"));
+// console.log(doubleChar("Hello")); 
+// console.log(doubleChar("1234!_ ")); 
+
