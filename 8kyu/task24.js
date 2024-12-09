3e180bdd0d800336c1cc243bf05fe27649d0fcae
maksicode <maksim-mih23@yandex.ru>
2024-12-09 17:45:56 +0300

    task24

--- 8kyu/task24.js
+++ 8kyu/task24.js
@@ -2,6 +2,15 @@
 // For example:
 // [1, 2, 3] --> [2, 4, 6]
 
+// function maps (array) {
+//     const array2 = [];
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i] * 2;
+//         array2.push(element);
+//     }
+//     return array2;
+// }
+
 function maps(x) {
     return x.map(n => n * 2);
 }
