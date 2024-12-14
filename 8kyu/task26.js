3b93c1787ee123c541262d6db714ea64cfde0aec
maksicode <maksim-mih23@yandex.ru>
2024-12-14 14:15:05 +0300

    task28

---
+++ 8kyu/task26.js
@@ -0,0 +1,11 @@
+// We need a function that can transform a number (integer) into a string.
+
+// function numberToString(num) {
+//     return String(num);
+//   }
+
+const numberToString = num => String(num);
+
+// const total = (numberToString(123));
+// console.log(total);
+// console.log(typeof total);
